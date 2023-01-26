78f48767532be62ce3828d4cb65a32509b3c9170
lclassicl <lclassicl86@gmail.com>
2023-01-26 14:06:10 +0900

    add handler

---
+++ src/main/command/MainHander.java
@@ -0,0 +1,16 @@
+package main.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mvc.command.CommandHandler;
+
+public class MainHander implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("메인핸들러 진입");
+		return "/view/main/main.jsp";
+	}
+
+}
