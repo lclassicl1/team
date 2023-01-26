e6985ee4822a074a7e545f3f9d354799e36a39ef
lclassicl <lclassicl86@gmail.com>
2023-01-26 21:39:31 +0900

    index 수정, dbconnection test 파일 추가, css default 파일추가

--- src/jdbc/conn/ConnectionProvider.java
+++ src/jdbc/conn/ConnectionProvider.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 //web.xml에서 설정한 poolName=board으로 지정한 board를 풀 이름으로 사용하여 Connection을 제공하는 클래스
 public class ConnectionProvider {
 	
-	public static Connection getConnection() throws SQLException {
+	public static Connection getConnection() throws SQLException {  
 		return DriverManager.getConnection("jdbc:apache:commons:dbcp:gosu");
 		
 	}
