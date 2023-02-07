43cecf70fae7df18edbcc9ac9d013b5f1e69908b
lclassicl <lclassicl86@gmail.com>
2023-02-07 18:53:34 +0900

    commit

--- src/auth/command/LogoutHandler.java
+++ src/auth/command/LogoutHandler.java
@@ -16,7 +16,7 @@ public class LogoutHandler implements CommandHandler {
 			session.invalidate();
 		}
 		
-		res.sendRedirect(req.getContextPath()+"index.jsp");
+		res.sendRedirect(req.getContextPath()+"/index.jsp");
 		return null;
 	}
 
